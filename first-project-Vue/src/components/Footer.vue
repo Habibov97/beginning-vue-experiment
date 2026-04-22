882914ff71fa2820b6b91d333d68aa2d8fc43927
Habibov97 <najaff.habibov@gmail.com>
2026-04-23 03:40:45 +0400

    mini repo for learning basic structure of vue. template, script, style, component layout.

---
+++ first-project-Vue/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+const year = ref(new Date().getFullYear());
+</script>
+
+<template>
+  <footer>copywrite {{ year }}</footer>
+</template>
+
+<style scoped>
+footer {
+  position: absolute;
+  color: blue;
+  bottom: 0;
+  padding-bottom: 2rem;
+  text-align: center;
+  width: 100%;
+  font-size: 2rem;
+}
+</style>
---
+++ first-project-Vue/src/components/Main.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+const name = ref('Rachel');
+</script>
+
+<template>
+  <main>
+    <h1>Hello {{ name }}</h1>
+    <p>
+      I'm about to learns
+      <a href="https://vuejs.org/guide/quick-start.html" target="_blank" rel="noopener">Vue.js</a>!
+    </p>
+  </main>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 5rem;
+  color: #333;
+}
+
+p {
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
